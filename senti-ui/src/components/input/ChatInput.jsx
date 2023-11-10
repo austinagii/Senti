987e0bf8de98806acc09b99a37dd2bbff8664bd2
Kadeem Austin <dev@kadeemaustin.ai>
2023-11-09 21:32:35 -0400

    chore: simplify project by merging api & core components

---
+++ senti-ui/src/components/input/ChatInput.jsx
@@ -0,0 +1,9 @@
+import "./chatInput.css"
+
+export default function ChatInput() {
+    return (
+        <div>
+            <input className="ChatInput-Input" type="text" />
+        </div>
+    )
+}
---
+++ senti-ui/src/components/input/ChatInput.stories.js
@@ -0,0 +1,11 @@
+import ChatInput from './ChatInput';
+
+export default {
+    component: ChatInput,
+    title: "ChatInput",
+    parameters: {
+        layout: "centered",
+    }
+}
+
+export const Default = () => <ChatInput />;
