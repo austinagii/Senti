63fb8e096000f6a00a7a605c83ab78a0d2d150c3
Kadeem Austin <dev@kadeemaustin.ai>
2023-11-12 23:53:16 -0500

    feat(api): add sample rest api with c lib bindings

--- senti-api/main.go
+++ senti-api/main.go
@@ -1,17 +1,37 @@
-package main 
+package main
 
-import "fmt"
+import (
+	// #include "add.h"
+	// #cgo LDFLAGS: -L. -ladd
+	"C"
+	"net/http"
 
-type textCompletion struct {
-    Text        string  `json:"text"`
-    Completion  string  `json:"completion"`
+	"github.com/gin-gonic/gin"
+)
+
+type addRequest struct {
+	A int `json:"a"`
+	B int `json:"b"`
 }
 
-var completions = []textCompletion{
-    {Text: "Hey there", Completion: " my name is kadeem"},
-    {Text: "Today is", Completion: " a wonderful day"},
+type addResponse struct {
+	Result int `json:"result"`
+}
+
+func add(c *gin.Context) {
+	var req addRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	sum := C.add(C.int(req.A), C.int(req.B))
+	result := addResponse{Result: int(sum)}
+	c.IndentedJSON(http.StatusOK, result)
 }
 
 func main() {
-    fmt.Println("Welcome Kadeem!")
+	router := gin.Default()
+	router.POST("/add", add)
+	router.Run("localhost:8080")
 }
