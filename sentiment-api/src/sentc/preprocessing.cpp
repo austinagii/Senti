4e12a5131d80cfb3d7d80d988d5f7208c1f67e8e
Kadeem Austin <dev@kadeemaustin.ai>
2023-11-19 16:15:19 -0400

    fix(sentiment-api): fix compilation errors in cpp source

--- sentiment-api/src/sentc/preprocessing.cpp
+++ sentiment-api/src/sentc/preprocessing.cpp
@@ -9,11 +9,11 @@
 #include "preprocessing.hpp"
 
 
-using json = nlohmann::json;
+// using json = nlohmann::json;
     
 senti::Tokenizer::Tokenizer(std::string path) {
     // TODO: Handle errors.
-    json token_by_id_json = json::parse(std::ifstream{path});
+    nlohmann::json token_by_id_json = nlohmann::json::parse(std::ifstream{path});
     token_by_id = token_by_id_json.get<std::map<std::string, unsigned short>>();
 }
 
@@ -32,10 +32,15 @@ std::vector<unsigned short> senti::Tokenizer::tokenize(std::string text) {
     return tokens;
 }
 
+std::size_t senti::Tokenizer::get_vocab_size() {
+    return token_by_id.size();
+}
+
 std::vector<torch::jit::IValue> senti::to_batch(std::vector<unsigned short> tokens, unsigned short vocab_size) {
     auto input = torch::zeros({1, vocab_size});
     for (auto token : tokens) {
         input[0][token] = 1;
     }
-    return std::vector<torch::jit::IValue> batch{input};
+    std::vector<torch::jit::IValue> batch{input};
+    return batch;
 }
--- sentiment-api/src/sentc/preprocessing.hpp
+++ sentiment-api/src/sentc/preprocessing.hpp
@@ -4,15 +4,15 @@
 
 #include <torch/script.h>
 
-using json = nlohmann::json;
 
 namespace senti {
-    std::vector<torch::jit::IValue> batch(std::vector<unsigned short> tokens, unsigned short vocab_size);
+    std::vector<torch::jit::IValue> to_batch(std::vector<unsigned short> tokens, unsigned short vocab_size);
 
     struct Tokenizer {
         std::map<std::string, unsigned short> token_by_id;
 
         Tokenizer(std::string path);
         std::vector<unsigned short> tokenize(std::string text);
+        std::size_t get_vocab_size();
     };
 }
